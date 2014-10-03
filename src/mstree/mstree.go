0ad20f306c35e8c60a28bf9a0f6ffcfe76e26136
Pavel Vorobyov <aquavitale@yandex.ru>
2014-10-03 16:30:57 +0400

    node in separate source file

--- src/mstree/mstree.go
+++ src/mstree/mstree.go
@@ -7,14 +7,10 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"regexp"
 	"strings"
 	"time"
 )
 
-type node struct {
-	Children map[string]*node
-}
 type MSTree struct {
 	indexDir    string
 	Root        *node
@@ -30,12 +26,7 @@ func (tce *TreeCreateError) Error() string {
 	return tce.msg
 }
 
-func newNode() *node {
-	return &node{make(map[string]*node)}
-}
-
 var (
-	// Package-wide logger
 	log *logging.Logger = logging.MustGetLogger("metricsearch")
 )
 
@@ -138,82 +129,6 @@ func loadWorker(idxFile string, idxNode *node, ev eventChan) {
 	}
 	log.Debug("<%s> loader finished", idxFile)
 	ev <- nil
-}
-
-func (n *node) insert(tokens []string, inserted *bool) {
-	if len(tokens) == 0 {
-		return
-	}
-	first, tail := tokens[0], tokens[1:]
-	child, ok := n.Children[first]
-	if !ok {
-		*inserted = true
-		child = newNode()
-		n.Children[first] = child
-	}
-	child.insert(tail, inserted)
-}
-
-func (n *node) traverseDump(prefix string, writer io.Writer) {
-	if len(n.Children) == 0 {
-		io.WriteString(writer, prefix+"\n")
-	} else {
-		for k, node := range n.Children {
-			var nPref string
-			if prefix == "" {
-				nPref = k
-			} else {
-				nPref = prefix + "." + k
-			}
-			node.traverseDump(nPref, writer)
-		}
-	}
-}
-
-func (n *node) search(pattern string) map[string]*node {
-	if pattern == "*" {
-		return n.Children
-	}
-
-	results := make(map[string]*node)
-
-	wcIndex := strings.Index(pattern, "*")
-	if wcIndex == -1 {
-		if node, ok := n.Children[pattern]; ok {
-			results[pattern] = node
-		}
-		return results
-	}
-	lwcIndex := strings.LastIndex(pattern, "*")
-	if wcIndex != lwcIndex || (wcIndex != 0 && wcIndex != len(pattern)-1) {
-		// more than one wildcard or one wildcard in the middle
-		re := regexp.MustCompile(strings.Replace(pattern, "*", ".*", -1))
-		for k, node := range n.Children {
-			if re.MatchString(k) {
-				results[k] = node
-			}
-		}
-		return results
-	}
-
-	if wcIndex == len(pattern)-1 {
-		// wildcard at the end
-		partial := pattern[:len(pattern)-1]
-		for k, node := range n.Children {
-			if strings.HasPrefix(k, partial) {
-				results[k] = node
-			}
-		}
-	} else {
-		// wildcard at the begining
-		partial := pattern[1:]
-		for k, node := range n.Children {
-			if strings.HasSuffix(k, partial) {
-				results[k] = node
-			}
-		}
-	}
-	return results
 }
 
 // TODO: channeled index writer
---
+++ src/mstree/node.go
@@ -0,0 +1,92 @@
+package mstree
+
+import (
+	// logging "github.com/op/go-logging"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type node struct {
+	Children map[string]*node
+}
+
+func newNode() *node {
+	return &node{make(map[string]*node)}
+}
+
+func (n *node) insert(tokens []string, inserted *bool) {
+	if len(tokens) == 0 {
+		return
+	}
+	first, tail := tokens[0], tokens[1:]
+	child, ok := n.Children[first]
+	if !ok {
+		*inserted = true
+		child = newNode()
+		n.Children[first] = child
+	}
+	child.insert(tail, inserted)
+}
+
+func (n *node) traverseDump(prefix string, writer io.Writer) {
+	if len(n.Children) == 0 {
+		io.WriteString(writer, prefix+"\n")
+	} else {
+		for k, node := range n.Children {
+			var nPref string
+			if prefix == "" {
+				nPref = k
+			} else {
+				nPref = prefix + "." + k
+			}
+			node.traverseDump(nPref, writer)
+		}
+	}
+}
+
+func (n *node) search(pattern string) map[string]*node {
+	if pattern == "*" {
+		return n.Children
+	}
+
+	results := make(map[string]*node)
+
+	wcIndex := strings.Index(pattern, "*")
+	if wcIndex == -1 {
+		if node, ok := n.Children[pattern]; ok {
+			results[pattern] = node
+		}
+		return results
+	}
+	lwcIndex := strings.LastIndex(pattern, "*")
+	if wcIndex != lwcIndex || (wcIndex != 0 && wcIndex != len(pattern)-1) {
+		// more than one wildcard or one wildcard in the middle
+		re := regexp.MustCompile(strings.Replace(pattern, "*", ".*", -1))
+		for k, node := range n.Children {
+			if re.MatchString(k) {
+				results[k] = node
+			}
+		}
+		return results
+	}
+
+	if wcIndex == len(pattern)-1 {
+		// wildcard at the end
+		partial := pattern[:len(pattern)-1]
+		for k, node := range n.Children {
+			if strings.HasPrefix(k, partial) {
+				results[k] = node
+			}
+		}
+	} else {
+		// wildcard at the begining
+		partial := pattern[1:]
+		for k, node := range n.Children {
+			if strings.HasSuffix(k, partial) {
+				results[k] = node
+			}
+		}
+	}
+	return results
+}
